39b980a0a7256baeea8faff6127af4bde7de98d5
Jacob Sima <simajacob2011@gmail.com>
2019-01-18 08:04:04 +0200

    Updates

---
+++ precache-manifest.31c885a8cc6baa072d9be0997cb1f03c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "13b13a3fb6b39013e194",
+    "url": "/React-CRUD-App_1/static/js/runtime~main.13b13a3f.js"
+  },
+  {
+    "revision": "23d85b4e0d86a95396eb",
+    "url": "/React-CRUD-App_1/static/js/main.23d85b4e.chunk.js"
+  },
+  {
+    "revision": "d4b518b860835af4ac6d",
+    "url": "/React-CRUD-App_1/static/js/1.d4b518b8.chunk.js"
+  },
+  {
+    "revision": "23d85b4e0d86a95396eb",
+    "url": "/React-CRUD-App_1/static/css/main.d764d451.chunk.css"
+  },
+  {
+    "revision": "d4b518b860835af4ac6d",
+    "url": "/React-CRUD-App_1/static/css/1.8c31515b.chunk.css"
+  },
+  {
+    "revision": "669036c526e5a711484f0ff2ed397aa2",
+    "url": "/React-CRUD-App_1/index.html"
+  }
+];
